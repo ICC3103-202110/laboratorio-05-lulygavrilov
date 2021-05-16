c94ecd039ff6c7c56863b17cf1cd73b935b3dcfd
lulygavri <lbgavrilov@miuandes.cl>
2021-05-15 23:35:50 -0400

    advances

--- view.js
+++ view.js
@@ -1,5 +1,6 @@
 const figlet = require('figlet')
 const chalk = require('chalk')
+const inquirer = requite('inquirer')
 
 //acá todo tiene que ser funcional
 
@@ -11,6 +12,34 @@ function getTitle(){
         })
     )
 }
+
+function getTable(number){
+    const {billAmount} = model
+    const {percentage} = model
+    const {tip} = model
+    const{total} = model
+    return [
+        {'Bill Amount':billAmount,'Tip (%)':percentage,'Tip':tip,'Total':total}
+    ]
+}
+
+function valueInput(input){
+    const {billAmount} = model
+    const {percentage} = model
+    return inquirer.prompt([
+        {
+            type: 'number',
+            name: 'bill amount',
+            message: 'Bill Amount?',
+        },
+        {
+            type: 'number',
+            name:'tip',
+            message: 'Tip(%)?',
+        },
+    ])
+}
+
 
 module.exports = {
     getTitle
